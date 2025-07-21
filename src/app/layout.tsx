895c45e350780c4b6a1cf60205d912f9fac56261
Zahra-Nfls <Anaflous.Zahra@outlook.com>
2025-07-22 01:06:33 +0200

    portfolio glow up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,85 +1,11 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Plus_Jakarta_Sans, DM_Serif_Text,Abril_Fatface, Playfair_Display, Bodoni_Moda, EB_Garamond, Caveat, Play, Lilita_One, Dancing_Script, Nanum_Gothic, Patrick_Hand, Indie_Flower } from "next/font/google";
 import "./globals.css"; 
 
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "ᴘᴏʀᴛꜰᴏʟɪᴏ",
-  description: "Had so much fun creating my portfolio",
-};
-
-const jakarta = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  weight: ["400", "700"], // Choose weights you need
-  display: "swap", // Prevents font flickering
-});
-
-const dm = DM_Serif_Text({
-  subsets: ["latin"],
-  weight: ["400"],
-  display: "swap",
-});
-
-const playfair = Playfair_Display({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  display: "swap",
-});
-
-
-const eb = EB_Garamond({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  display: "swap",
-});
-
-const caveat = Caveat({
-  subsets: ["latin"],
-  weight: ["400"],
-  display: "swap",
-});
-
-const playFont = Play({
-  subsets: ["latin"],
-  weight: ["400"],
-  display: "swap",
-});
-
-const lilita = Lilita_One({
-  subsets: ["latin"],
-  weight: ["400"],
-  display: "swap",
-});
-
-const dancing = Dancing_Script({
-  subsets: ["latin"],
-  weight: ["400"],
-  display: "swap",
-});
-
-const nanum = Nanum_Gothic({
-  subsets: ["latin"],
-  weight: ["400"],
-  display: "swap",
-});
-
-const handwritten = Patrick_Hand({
-  subsets: ["latin"],
-  weight: ["400"],
-  display: "swap",
-});
-
-const indie = Indie_Flower({
-  subsets: ["latin"],
-  weight: ["400"],
-  display: "swap",
-});
-
-const abril = Abril_Fatface({ subsets: ["latin"], weight: ["400"] });
 
 export default function RootLayout({
   children,
@@ -88,7 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="/favicon.ico" />
+    <title>• 𝐊𝐚𝐫𝐦𝐚</title>
+    </head>
       <body className={inter.className}>
       <div className="font-indie">
           <main className="flex-grow">

d1f86e74286e4820384a60f9bf67d9ed09457bb6
Zahra-Nfls <Anaflous.Zahra@outlook.com>
2025-07-23 01:36:18 +0200

    portfolio glow up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,12 @@ export default function RootLayout({
     <html lang="en">
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
     <title>• Nfls</title>
+      <meta property="og:title" content="Zahra’s Portfolio" />
+        <meta property="og:description" content="Web developer portfolio built with Next.js, TypeScript, and Tailwind CSS" />
+        <meta property="og:image" content="https://karma-nfls-portfolio.vercel.app/images/Screenshot portfolio.png" />
+        <meta property="og:url" content="https://karma-nfls-portfolio.vercel.app" />
+        <meta property="og:type" content="website" />
     </head>
       <body className={inter.className}>
       <div className="font-indie">
